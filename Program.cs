4951e3cf733746c74021c28770c510e4c75a99df
sap1ks <sap1kcsgo@gmail.com>
2024-12-24 08:58:53 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Задача 1. Напечатать ряд чисел 20 в виде: 20 20 20 20 20 20 20 20 20 20.
+/// </summary>
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }     
+        static void Func()
+        {
+            for (int x = 0; x <10; x++)
+            {
+                Console.Write($"{20}\t");
+            }
+        }
+    }
+}
